4b500102317ba2d3da864b5dc7b31e4054db1bd9
thuctapnhomthu2 <48174126+thuctapnhomthu2@users.noreply.github.com>
2019-03-23 20:25:22 +0700

    Add files via upload

---
+++ manager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class manager : Form
+    {
+        public manager()
+        {
+            InitializeComponent();
+        }
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void thôngTinCáNhânToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Accounts a = new Accounts();
+            a.ShowDialog();
+        }
+
+        private void adminToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formAdmin f = new formAdmin();
+            f.ShowDialog();
+        }
+
+        private void accountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
